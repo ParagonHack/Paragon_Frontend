4ff3f45f2c72ec2608da0986205322e95932f774
Artur112 <artur.jurgenson@hotmail.com>
2023-06-23 13:34:53 +0200

    code to load a metadata file:

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,8 +1,25 @@
 import { Authenticator } from "@aws-amplify/ui-react";
 import Header from "../components/header";
 import VideoPlayer from "../components/video_player";
+import createS3Object from '../helpers/s3Object'
+import GetObjectCommand from 'aws-sdk/clients/s3'
 
 export default function Home() {
+
+    const loadVideoDescription = async () => {
+        const s3 = await createS3Object()
+        const url = s3.getSignedUrl('getObject', {Bucket: s3.config.Bucket, Key: 'videos/video_id=12345/metadata.json'})
+        const json = await fetch(url)
+            .then((response) => response.json())
+            .catch((error) => {
+              console.error(error)
+            })
+        console.log(json)
+        return json
+  }
+
+  loadVideoDescription()
+
   return (
     <div>
     <Header />
@@ -19,9 +36,10 @@ export default function Home() {
         {({ signOut, user }) => (
           <main>
             <h1>Hello, {user.username}!</h1>
-            <div style={{width: '100%'}}>
+              {/*<div style={{width: '100%'}}>
                 <VideoPlayer fileKey='videos/video_id=12345/video.mp4' />
             </div>
+            */}
             <button onClick={signOut}>Sign out</button>
           </main>
         )}
