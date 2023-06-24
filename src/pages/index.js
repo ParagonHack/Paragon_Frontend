53c3a2b098fc41f1a96f485ad3987008bd14e203
Misha <mihail.baltacov@gmail.com>
2023-06-24 23:35:58 +0100

    Changed the index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,11 @@
 import Header from "../components/header";
-import Dashboard from "../components/dashboard";
-import {Authenticator} from "@aws-amplify/ui-react";
-import { selectAuthState, setAuthState, setInitialState, setChatHistory } from "../store/slices";
-import { useDispatch, useSelector } from "react-redux";
-import { Auth } from 'aws-amplify';
-import { useEffect } from 'react';
-import dataManager from '../helpers/dataManager'
+import Carousel from "../components/carousel";
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-screen w-screen">
-     <Header/>
-       
+    <div className="flex flex-col h-screen w-screen ">
+      <Header />
+      <Carousel />
     </div>
-  )
+  );
 }
